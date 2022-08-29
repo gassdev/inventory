81a4c9231f43fcf152ffeefe1216bd8581cbe5f1
gassdev <gassdev@gmail.com>
2022-08-29 14:23:35 +0000

    add product and order models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Order
 
-# Register your models here.
+admin.site.site_header = 'Inventory System'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'qty')
+    list_filter = ('category',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+CATEGORY = (
+    ('Furniture', 'Furniture'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+)
+
+class Product(models.Model):
+    name=models.CharField(max_length=100, null=True)
+    category=models.CharField(max_length=20, choices=CATEGORY, null=True)
+    qty=models.PositiveIntegerField(null=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}-{self.qty}"
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    order_qty = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.product} ordered by {self.staff.username}"
